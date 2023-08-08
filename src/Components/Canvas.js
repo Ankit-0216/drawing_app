6f6f9a5575c4f8c212b04e13b3938159d375c2c9
Ankit-0216 <125597650+Ankit-0216@users.noreply.github.com>
2023-08-08 15:36:46 +0530

    Add files via upload

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { useOnDraw } from "./Hooks";
 
-const Canvas = ({ width, height }) => {
-  const setCanvasRef = useOnDraw(onDraw);
+const Canvas = ({ width, height, backgroundImageUrl }) => {
+  const { setCanvasRef, onCanvasMouseDown } = useOnDraw(onDraw);
 
   function onDraw(ctx, point, prevPoint) {
     drawLine(prevPoint, point, ctx, "#000000", 5);
@@ -22,18 +23,25 @@ const Canvas = ({ width, height }) => {
     ctx.fill();
   }
 
+  const canvasStyle = {
+    border: "1px solid black",
+    width: "700px",
+    height: "500px",
+    backgroundImage: `url('${backgroundImageUrl}')`,
+    backgroundSize: "cover", // T0 cover the canvas entirely
+  };
+
   return (
-    <canvas
-      width={width}
-      height={height}
-      style={canvasStyle}
-      ref={setCanvasRef}
-    />
+    <div style={{ width: "100%", height: "100%" }}>
+      <canvas
+        width={width}
+        height={height}
+        onMouseDown={onCanvasMouseDown}
+        style={canvasStyle}
+        ref={setCanvasRef}
+      />
+    </div>
   );
 };
 
 export default Canvas;
-
-const canvasStyle = {
-  border: "1px solid black",
-};
